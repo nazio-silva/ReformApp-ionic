d885f52740f0ff7f6e2450d0f0cfa8eb204225e0
Nazio silva <nazio.silva@gmail.com>
2017-11-27 16:43:17 -0300

    Calculando Tijolos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,24 +3,41 @@ import { FirebaseListObservable } from 'angularfire2';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
+import { NgForm } from '@angular/forms';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  public cal;
-
+  public area : number;
+  public preco : number;
+  
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
   ) {
-    this.cal = this.navParams.data;
+    
   }
 
-  calcular(b) {
-    console.log(this.cal, {
-      nome:'joao'
-    });
+  calcular() {
+    var qtd: number = Math.round((this.area * 1 ) * 27.5);
+    console.log(qtd);
+
+    var gasto: number = Math.round((this.preco/1000 * this.area * 1 ) * 27.5);
+    console.log(gasto);
+
   }
 }
+
+
+/*public Double getQuantidade(){
+  return (this.area * 27.5);
+}
+public Double getPrecoDoTijolo(){
+  return this.preco / 1000;		
+}
+public Double getValorGastoTijolo(){
+  return (getPrecoDoTijolo()*getQuantidade());
+}*/

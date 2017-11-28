fe31e579587696d363bdcb289fff67d8b3f7184e
Nazio silva <nazio.silva@gmail.com>
2017-11-28 12:22:30 -0300

    Exibindo resultado dos calculos de tijolo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,31 +13,43 @@ export class HomePage {
 
   public area : number;
   public preco : number;
+
+  public qtd = 0;
+  public gasto = 0.00;
   
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
   ) {
-    
+   
   }
 
   calcular() {
-    var qtd: number = Math.round((this.area * 1 ) * 27.5);
-    console.log(qtd);
+    var quantidade_tijolo: number = Math.round((this.area * 1 ) * 27.5);
+    this.qtd = quantidade_tijolo;
+    console.log("Quantidade:" , quantidade_tijolo);
 
-    var gasto: number = Math.round((this.preco/1000 * this.area * 1 ) * 27.5);
-    console.log(gasto);
-
+    var gasto_tijolo: number = (this.preco/1000 * this.area * 1 ) * 27.5;
+    this.gasto = gasto_tijolo.toFixed(2);
+    console.log("Valor Gasto:" , this.gasto);
   }
 }
 
 
-/*public Double getQuantidade(){
-  return (this.area * 27.5);
-}
-public Double getPrecoDoTijolo(){
-  return this.preco / 1000;		
-}
-public Double getValorGastoTijolo(){
-  return (getPrecoDoTijolo()*getQuantidade());
-}*/
+/*
+
+  var teste = ${nomeVariavelVindoAction}
+ai sua variavel javascript ja virá com o valor que vc mandou da action.
+
+
+
+  public Double getQuantidade(){
+    return (this.area * 27.5);
+  }
+  public Double getPrecoDoTijolo(){
+    return this.preco / 1000;		
+  }
+  public Double getValorGastoTijolo(){
+    return (getPrecoDoTijolo()*getQuantidade());
+  }
+*/
